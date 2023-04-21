b075f0ef520007a872284e95c0485fc66c67d5f6
Yelyzaveta Berest <lizaberest97@gmail.com>
2023-04-21 16:02:38 +0300

    node calculator

---
+++ 23-node_npm_Calculator-hw/src/calculator/index.js
@@ -0,0 +1,12 @@
+const {add} = require('./add.js')
+const {sub} = require('./sub.js')
+const {mult} = require('./mult.js')
+const {div} = require('./div.js')
+
+
+
+module.exports.add = add
+module.exports.sub = sub
+module.exports.mult = mult
+module.exports.div = div
+
